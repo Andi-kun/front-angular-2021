2836e56d6b48c5e420d35d5425f40bfb26d3aa1b
Andi <rasoanaivonytokyandi@gmail.com>
2021-04-06 12:22:49 +0300

    change prod backend domain

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiDomain : "https://back-node-2021.herokuapp.com",
+  apiDomain : "back-node-2021.herokuapp.com",
   apiUri : "https://back-node-2021.herokuapp.com/api"
 };
